8cd537945d22b21414dc7fdd3ecd55dd19044436
MargoTech <margobartashevich@gmail.com>
2025-07-06 13:47:21 -0100

    feat: extended user fetch with filter,sort,output

--- async/fetch-api-users.js
+++ async/fetch-api-users.js
@@ -3,8 +3,19 @@ async function fetchUsers() {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await response.json();
     for (const user of users) {
-      console.log(user.name);
+      console.log(
+        `Name: ${user.name} , E-mail ${user.email} , City: ${user.address.city}`
+      );
     }
+    const filtered = users.filter(
+      (user) => user.name.startsWith("C") || user.name.startsWith("E")
+    );
+    const sorted = filtered.sort((a, b) => a.name.localeCompare(b.name));
+    for (const user of sorted) {
+      console.log(`🔍${user.name}, ${user.email}, ${user.address.city}`);
+    }
+    const longNames = users.filter((user) => user.name.length > 10).length;
+    console.log(`📏 Users with long names (>10): ${longNames}`);
   } catch (error) {
     console.log("error", error);
   }
