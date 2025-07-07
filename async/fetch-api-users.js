7d27bcf65c83d27824ba0abf02170ee3353ea514
MargoTech <margobartashevich@gmail.com>
2025-07-07 13:52:18 -0100

    feat: grouping users by city

--- async/fetch-api-users.js
+++ async/fetch-api-users.js
@@ -7,15 +7,32 @@ async function fetchUsers() {
         `Name: ${user.name} , E-mail ${user.email} , City: ${user.address.city}`
       );
     }
+
     const filtered = users.filter(
       (user) => user.name.startsWith("C") || user.name.startsWith("E")
     );
+
     const sorted = filtered.sort((a, b) => a.name.localeCompare(b.name));
+
     for (const user of sorted) {
       console.log(`🔍${user.name}, ${user.email}, ${user.address.city}`);
     }
+
     const longNames = users.filter((user) => user.name.length > 10).length;
     console.log(`📏 Users with long names (>10): ${longNames}`);
+
+    const groupByCity = {};
+    for (const user of users) {
+      const city = user.address.city;
+      if (!groupByCity[city]) {
+        groupByCity[city] = [];
+      }
+      groupByCity[city].push(user);
+      for (const city in groupByCity) {
+        const count = groupByCity[city].length;
+        console.log(`🏙️${city} → ${count} users`);
+      }
+    }
   } catch (error) {
     console.log("error", error);
   }
