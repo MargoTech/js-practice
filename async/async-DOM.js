c799675c248d74a3005d59d73fd1d9111cd4dd56
MargoTech <margobartashevich@gmail.com>
2025-07-10 15:08:36 -0100

    Working with async-DOM.js and index.html

--- async/async-DOM.js
+++ async/async-DOM.js
@@ -2,7 +2,12 @@ async function fetchUsers() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await response.json();
-    const some = document.getElementById("users");
+    const container = document.getElementById("users");
+    for (const user of users) {
+      const userDiv = document.createElement("div");
+      userDiv.innerText = `👤 Name: ${user.name} | 📧 Email: ${user.email}  | 🏙️ City: ${user.address.city}`;
+      container.appendChild(userDiv);
+    }
   } catch (error) {
     console.log("error", error);
   }
